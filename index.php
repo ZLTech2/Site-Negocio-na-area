7e0e8db89c3cf05be094c92d7cd6bcdd764283e3
ZLTech2 <zltech64@gmail.com>
2024-11-16 12:01:28 -0300

    [refactor] criando header.php

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Negócio na Área</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="./bootstrap-5.3.3-examples/carousel/carousel.css" rel="stylesheet">
+    <link rel="shortcut icon" href="./assets/images/logo rd (1).png" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul class="nav-list">
+                <div class="logo">
+                    <a href="index.php"><img src="./assets/images/logo rd (1).png" alt=""></a>
+                </div>
+                <li><a href="./php/personalize.php">Personalize</a></li>
+                <li><a href="#sobre">Sobre</a></li>
+                <li><a href="#">ZL Tech</a></li>
+                <li> <a href="./php/cadastro.php">Cadastrar</a></li>
+                <li><a href="./php/login.php">Entrar</a></li>
+            </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg> -->
+                <!-- <img src="./assets/images/fundo_roxo.jpg" alt="" width="100%" height="100%"> -->
+                <div class="container">
+                  <div class="carousel-caption text-start">
+                    <span class="color-word">Crie um site </span><span> para sua loja com nosso</span> <span class="color-word">layout </span> <span> predefinido</span>
+                    </span>  
+                    <p class="opacity-75"></p>
+                    <p><a class="btn btn-lg btn-primary" href="./php/cadastro.php">Cadastre-se</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg> -->
+                <!-- <img src="./assets/images/fundo_roxo.jpg" alt="" srcset="" width="100%" height="100%"> -->
+                <div class="container">
+                  <div class="carousel-caption">
+                    <h1></h1>
+                    <span class="color-word">Disponível em </span> <span>todos os dispositivos, como </span><span class="color-word"> celular e desktop</span>
+                    <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <!-- <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg> -->
+                <!-- <img src="./assets/images/fundo_roxo.jpg" alt="" srcset="" width="100%" height="100%"> -->
+                <div class="container">
+                  <div class="carousel-caption text-end">
+                    <!-- <h1>One more for good measure.</h1> -->
+                    <span class="color-word">Melhore a divulgação</span> <span>da sua loja com nosso </span><span class="color-word">suporte </span> <span>e </span><span class="color-word"> rede da comunidade</span>
+                    <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+
+        <!-- <section id="slideshow">
+            <span id="slideshow-text">
+                <span class="color-word">Crie um site</span> para sua loja com nosso <span class="color-word">layout</span> pré definido
+            </span>
+            <img id="slideshow-image" src="./assets/images/logo rd (1).png" alt="">
+        </section> -->
+        
+
+        <section id="sobre">
+            <h1>Alavanque suas vendas</h1>
+            <span>Crie seus próprios layouts</span>
+            <img src="./assets/images/logo rd (1).png" alt="">
+            <h2>Divulgue sua empresa</h2>
+            <span>Cresça no mercado digital</span>
+            <span></span>
+            <button type="submit">Saiba mais</button>
+        </section>
+
+
+        <footer class="rodape">
+            <div class="empresa">
+                <span>Sobre</span>
+                <span>ZL Tech</span>
+            </div>
+            <div class="redes-sociais">
+                <span><a href="#"><i class="fa-brands fa-facebook"></i></a></span>
+                <span><a href="#"><i class="fa-brands fa-instagram"></i></a></span>
+                <span><a href="#"><i class="fa-brands fa-github"></i></a></span>
+                <span><a href="#"><i class="fa-brands fa-youtube"></i></a></span>
+                <!-- <div class="botao">
+                    <button>Entre em contato</button>
+                </div> -->
+            </div>
+            
+        </footer>
+    </main>
+    
+    <script src="./js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="../css/styles.css">
+<header>
+    <nav>
+        <ul class="nav-list">
+            <div class="logo">
+                <img src="../assets/images/logo rd (1).png" alt="">
+            </div>
+            <li><a href="#personalize">Personalize</a></li>
+            <li><a href="#sobre">Sobre</a></li>
+            <li><a href="zl.tech.com">ZL Tech</a></li>
+            <li> <a href="./php/cadastro.php">Cadastrar</a></li>
+            <li><a href="./php/login.php">Entrar</a></li>
+        </ul>
+        </div>
+    </nav>
+</header>
